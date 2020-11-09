28f893d8a5f5ac3a574203eb192bb02b1511a73c
MLeandroCardenas <mleandrocardenas@ucundinamarca.edu.co>
2020-11-09 03:57:24 -0500

    Update ILibroService.java

--- src/main/java/com/udec/service/ILibroService.java
+++ src/main/java/com/udec/service/ILibroService.java
@@ -2,5 +2,5 @@ package com.udec.service;
 import com.udec.entity.Libro;
 import com.udec.repository.IOperacionesGenericas;
 
-public interface ILibroService extends IOperacionesGenericas<Libro, Integer> {
-}
+/*public interface ILibroService extends IOperacionesGenericas<Libro, Integer> {
+}*/
