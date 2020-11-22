2da52cd0b80994cd17939a3f21452236c643a769
MLeandroCardenas <mleandrocardenas@ucundinamarca.edu.co>
2020-11-22 12:45:45 -0500

    tareas programadas

---
+++ src/main/java/com/udec/schedule/Programado.java
@@ -0,0 +1,14 @@
+package com.udec.schedule;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Programado {
+	
+	@Scheduled(fixedRate = 5000)
+	public void ejecutar() {
+		System.out.println("Bienvenidos alumnos....");
+	}
+
+}
